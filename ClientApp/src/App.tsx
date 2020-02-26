e1dcf5968bbd6bbdd8ff8b376e5c51d2356f8abc
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-02-26 12:40:24 -0500

    added typescript

---
+++ ClientApp/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './custom.css'
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <div>
+      hello
+      </div>
+    );
+  }
+}
