14e0bd1e5e499edcce4d27409421481c74263bd2
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-03-19 15:06:15 -0400

    added axios interceptor

--- ClientApp/src/app/api/axiosagent.ts
+++ ClientApp/src/app/api/axiosagent.ts
@@ -1,8 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
 import { IHolding } from '../models/IHolding';
 import { ILoginUser, IRegisterUser, IUser } from '../models/IUsers';
+import { history } from '../../index';
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'http://localhost:5002/api/';
+
+axios.interceptors.response.use(undefined, (error) => {
+    const { status, config, data } = error.response;
+    if (status === 404 || (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')))
+        history.push('/notfound');
+    else if(status === 500)
+    toast.error('Server error - check terminal for more info')
+})
 const responseBody = (response: AxiosResponse) => response.data;
 
 const requests = {
