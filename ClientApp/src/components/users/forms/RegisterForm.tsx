78b387a4ea741cb3fd4a2d716c4f02ef8b8b17bc
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-04-12 10:30:01 -0400

    added formik to morms

--- ClientApp/src/components/users/forms/RegisterForm.tsx
+++ ClientApp/src/components/users/forms/RegisterForm.tsx
@@ -1,33 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { Form, Button, Spinner } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import { Form, Button, Spinner } from 'react-bootstrap';
 import { IRegisterUser, IUser } from '../../../app/models/IUsers';
-import axiosagent from '../../../app/api/axiosagent'
+import axiosagent from '../../../app/api/axiosagent';
 import { history } from '../../../index';
+import { useFormik } from 'formik';
 
 interface IProps {
     setUser: (user: IUser) => void;
 }
+
 export const RegisterForm: React.FC<IProps> = ({ setUser }) => {
-    let [registerUser, setRegisterUser] = useState<IRegisterUser>({displayName:"", email:"", userName:"", password:""});
     let [spinning, setSpinning] = useState<boolean>(false);
-    let [submitDisabled, setSubmitDisabled] = useState<boolean>(true);
     let [loggedIn, setLoggedIn] = useState<boolean>(false);
+
+    let initialValues: IRegisterUser = { displayName: "", email: "", userName: "", password: "" };
 
     useEffect(() => {
         if (loggedIn) history.push('/');
+    }, [loggedIn]);
 
-        let isEmailInvalid = registerUser.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) ? false : true;
-        setSubmitDisabled(registerUser.password.length < 6 || isEmailInvalid 
-            || registerUser.displayName.length < 1 || registerUser.userName.length < 1);
-    }, [registerUser, loggedIn]);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let { name, value } = e.target;
-        setRegisterUser({ ...registerUser, [name]: value });
-    }
-
-    const handleRegistration = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault();
+    const handleRegistration = (registerUser:IRegisterUser) => {
         setSpinning(true);
         axiosagent.UsersRequests.register(registerUser)
             .then((response: IUser) => {
@@ -39,38 +31,64 @@ export const RegisterForm: React.FC<IProps> = ({ setUser }) => {
             .finally(() => setSpinning(false));
     }
 
-    const clearForm = () => {
-        setRegisterUser({
-            email: '',
-            password: '',
-            displayName:'',
-            userName:''
-        })
-    }
+    const formik = useFormik({
+        initialValues: initialValues,
+        onSubmit: (values, actions) => {
+            actions.resetForm();
+            handleRegistration(values);
+        }
+    });
+
     return (
-        <Form>
-             <Form.Group> 
+        <Form onSubmit={formik.handleSubmit}>
+            <Form.Group>
                 <Form.Label>User Name</Form.Label>
-                <Form.Control required type="text" placeholder="Enter User Name" name="userName" onChange={handleChange} value={registerUser.userName} />
-                <div className="text-danger">{registerUser.userName.length < 1 ? "User name must contain 1 character at least" : null} </div>
+                <Form.Control
+                    placeholder="Enter User Name"
+                    name="userName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.userName}
+                />
+                {(formik.touched.userName && formik.errors.userName) && <div className="text-danger">{formik.errors.userName} </div>}
             </Form.Group>
 
-            <Form.Group> 
+            <Form.Group>
                 <Form.Label>Display Name</Form.Label>
-                <Form.Control required type="text" placeholder="Enter Display Name" name="displayName" onChange={handleChange} value={registerUser.displayName} />
-                <div className="text-danger">{registerUser.displayName.length < 1 ? "Display Name must contain 1 character at least" : null} </div>
+                <Form.Control
+                    placeholder="Enter Display Name"
+                    name="displayName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.displayName}
+                />
+                {(formik.touched.displayName && formik.errors.displayName) && <div className="text-danger">{formik.errors.displayName} </div>}
             </Form.Group>
 
             <Form.Group>
                 <Form.Label>Email</Form.Label>
-                <Form.Control required type="email" placeholder="Enter Email" name="email" onChange={handleChange} value={registerUser.email} />
-                <div className="text-danger">{registerUser.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) ? null : "Must enter valid email"}</div>
+                <Form.Control
+                    placeholder="Enter Email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.email} />
+                {(formik.touched.email && formik.errors.email) && <div className="text-danger">{formik.errors.email} </div>}
             </Form.Group>
 
-            <Form.Group> 
+            <Form.Group>
                 <Form.Label>Password</Form.Label>
-                <Form.Control required type="password" placeholder="Enter Password" name="password" onChange={handleChange} value={registerUser.password} />
-                <div className="text-danger">{registerUser.password.length < 6 ? "Password must contain 6 characters at least" : null} </div>
+                <Form.Control
+                    placeholder="Enter Password"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password} />
+                {(formik.touched.password && formik.errors.password) && <div className="text-danger">{formik.errors.password} </div>}
             </Form.Group>
             {spinning ?
                 <div className="d-flex justify-content-center">
@@ -84,10 +102,10 @@ export const RegisterForm: React.FC<IProps> = ({ setUser }) => {
                 </div>
                 :
                 <div className="mb-2">
-                    <Button style={{ float: 'right' }} type='submit' variant="primary" size="lg" onClick={handleRegistration} disabled={submitDisabled}>
+                    <Button style={{ float: 'right' }} type='submit' variant="primary" size="lg"  >
                         Register
                     </Button>{' '}
-                    <Button style={{ float: 'right' }} variant="secondary" size="lg" onClick={clearForm}>
+                    <Button style={{ float: 'right' }} variant="secondary" size="lg" onClick={() => formik.resetForm()}>
                         Clear Form
                     </Button>
                 </div>
